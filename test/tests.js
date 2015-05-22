e36e06dc02da66de5f6b65790b387d0c2df1d77c
Andrea Stagi <stagi.andrea@gmail.com>
2015-05-22 16:10:53 +0200

    Add some tests

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,38 @@
 module("fiji");
 
 test( "init test", function( assert ) {
-    assert.equal( 0, 0, "Zero, Zero; equal succeeds" );
+    var f = fiji({
+        plain: "text"
+    });
+    assert.equal( typeof(f.run), "function", "Return callable function" );
+});
+
+test( "target function called", function( assert ) {
+    var called = assert.async();
+    function main(context) {
+        assert.equal(context.plain, "text", "Target function is called.");
+        called();
+    };
+    fiji({
+        plain: "text"
+    }).run(main);
+});
+
+test( "parse plain", function( assert ) {
+    var called = assert.async();
+    function main(context) {
+        assert.equal(
+            context.plain_text, "plain text",
+            "Plain string remains plain string."
+        );
+        assert.equal(
+            context.plain_number, 1,
+            "Plain numbers remains plain numbers."
+        );
+        called();
+    };
+    fiji({
+        plain_text: "plain text",
+        plain_number: 1,
+    }).run(main);
 });
